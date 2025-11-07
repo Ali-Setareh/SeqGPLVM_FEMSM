7e7ed58bba4ca60c2b360221310ecf52ff284f7e
Ali-Setareh <ali.setarehkokab97@gmail.com>
2025-11-07 12:51:01 +0100

    Add dataset_fingerprint function for generating data frame fingerprints

---
+++ utils/dataset_fingerprint.py
@@ -0,0 +1,20 @@
+import hashlib
+import pandas as pd
+import numpy as np
+
+def dataset_fingerprint(df: pd.DataFrame) -> dict:
+    cols = list(df.columns)
+    dtypes = [str(df[c].dtype) for c in cols]
+    rowhash = pd.util.hash_pandas_object(df, index=True, categoricals=True)
+
+    h = hashlib.sha256()
+    h.update(np.int64(len(df)).tobytes())
+    h.update(",".join(cols).encode())
+    h.update(",".join(dtypes).encode())
+    h.update(rowhash.values.tobytes())
+    return {
+        "algo": "sha256",
+        "n_rows": int(len(df)),
+        "n_cols": int(df.shape[1]),
+        "hexdigest": h.hexdigest(),
+    }
